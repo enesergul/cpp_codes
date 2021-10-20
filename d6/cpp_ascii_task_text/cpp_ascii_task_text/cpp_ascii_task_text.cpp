c5ddcbc83effef2073492899f208db147c9944b7
enesergul <92294382+enesergul@users.noreply.github.com>
2021-10-20 10:19:15 +0300

    Add files via upload

---
+++ d6/cpp_ascii_task_text/cpp_ascii_task_text/cpp_ascii_task_text.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+
+int main()
+{
+	fstream myFile;
+	myFile.open("ascii.txt", ios::in); //write mode
+	if (myFile.is_open()) {
+		string line;
+		while (getline(myFile,line) ){
+		
+			cout << line << endl;
+		
+	}
+
+	
+	}
+
+	
+		
+
+		myFile.close();
+	}
+	
+
+
+
+
+
+
---
+++ d6/cpp_filess/cpp_filess/cpp_filess.cpp
@@ -0,0 +1,37 @@
+﻿// cpp_filess.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
+//
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+
+int main()
+{
+	fstream myFile;
+	myFile.open("enes.txt", ios::out); //write mode
+	if (myFile.is_open()) {
+
+		myFile << "Hello \n";
+		myFile << "merhabaaaaaa \n";
+
+		myFile.close();
+	}
+  myFile.open("enes.txt", ios::app); //write mode
+	if (myFile.is_open()) {
+
+			
+			myFile << "asdasdsadsad \n";
+
+			myFile.close();
+
+
+
+	}
+	
+
+	//system("pause>0");
+
+}
+
